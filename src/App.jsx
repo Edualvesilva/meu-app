a734da3b2166a301db8b0202eebf44d692b8da9a
Eduardo Alves da Silva <eduardoalvniver@gmail.com>
2023-11-29 11:17:38 -0300

    Componente único

--- src/App.jsx
+++ src/App.jsx
@@ -13,10 +13,36 @@ function App() {
         </nav>
       </header>
       <main>
-        <h2>Teste</h2>
+        <section>
+          <h2>Conteúdo da aplicação</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Adipisci
+            magnam quo ducimus dolores, necessitatibus earum sit at rem, qui,
+            impedit voluptatibus inventore temporibus pariatur quidem!
+            Accusantium architecto veritatis velit rerum?
+          </p>
+          <article>
+            <h3>Artigo 1</h3>
+            <p>Conteúdo do artigo 1</p>
+          </article>
+
+          <article>
+            <h3>Artigo 2</h3>
+            <p>Conteúdo do artigo 2</p>
+          </article>
+
+          <article>
+            <h3>Artigo 3</h3>
+            <p>Conteúdo do artigo 3</p>
+          </article>
+        </section>
       </main>
+      <hr />
+      <footer>
+        <h2>Rodapé da aplicação</h2>
+      </footer>
     </>
-  )
+  );
 }
 
 export default App;
