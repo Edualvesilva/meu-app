63e1dafd3ff8923103107a08cd149fd597668955
Eduardo Alves da Silva <eduardoalvniver@gmail.com>
2023-11-30 10:55:16 -0300

    Classes com o mesmo nome e como chamar

--- src/components/Cabecalho.jsx
+++ src/components/Cabecalho.jsx
@@ -1,12 +1,17 @@
 import Menu from "./Menu";
-function Cabecalho(){
-    return (    
-        <header>
-        <h1>Olá React!</h1>
-        <hr />       
-        <Menu />
-      </header>
-    )
+
+/* Importação do módulo CSS como um objeto chamado estilos (o nome é a seu critério) */
+import style from "../components/Cabecalho.module.css";
+function Cabecalho() {
+  return (
+    <header>
+      <h1>
+        Olá <span className={style.sombra}>React!</span>
+      </h1>
+      <hr />
+      <Menu />
+    </header>
+  );
 }
 
 export default Cabecalho;
