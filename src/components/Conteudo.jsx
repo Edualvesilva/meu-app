540814d1d9a6aefec498f6be1fbee6784445a881
Eduardo Alves da Silva <eduardoalvniver@gmail.com>
2023-12-01 09:53:07 -0300

    Stylization of Conteudo

--- src/components/Conteudo.jsx
+++ src/components/Conteudo.jsx
@@ -1,20 +1,32 @@
+import styled from "styled-components";
 import Artigo from "./Artigo";
+
+const StyleMain = styled.main`
+  width: 90vw;
+  margin: 1rem auto;
+  background-color: aliceblue;
+  padding: 1rem;
+  box-shadow: darkblue 0 0 1px inset;
+
+  h2,
+  p {
+    padding: 0.5rem 0;
+  }
+`;
 function Conteudo() {
-    return(
-        <main>
-        <section>
-          <h2>Conteúdo da aplicação</h2>
-          <p>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Adipisci
-            magnam quo ducimus dolores, necessitatibus earum sit at rem, qui,
-            impedit voluptatibus inventore temporibus pariatur quidem!
-            Accusantium architecto veritatis velit rerum?
-          </p>
-        <Artigo />
-        <Artigo />
-        <Artigo />
-        </section>
-      </main>
-    )
+  return (
+    <StyleMain>
+      <h2>Conteúdo da aplicação</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Adipisci
+        magnam quo ducimus dolores, necessitatibus earum sit at rem, qui,
+        impedit voluptatibus inventore temporibus pariatur quidem! Accusantium
+        architecto veritatis velit rerum?
+      </p>
+      <Artigo />
+      <Artigo />
+      <Artigo />
+    </StyleMain>
+  );
 }
 export default Conteudo;
