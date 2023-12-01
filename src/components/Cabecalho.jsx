8f77f2de30426fd60247176476c867c0c44e62ed
Eduardo Alves da Silva <eduardoalvniver@gmail.com>
2023-12-01 09:17:16 -0300

    Update Cabecalho.jsx

--- src/components/Cabecalho.jsx
+++ src/components/Cabecalho.jsx
@@ -1,12 +1,21 @@
 import Menu from "./Menu";
-function Cabecalho(){
-    return (    
-        <header>
-        <h1>Olá React!</h1>
-        <hr />       
-        <Menu />
-      </header>
-    )
+
+/* Importação da lib Styled Components */
+import styled from "styled-components";
+
+const StyledHeader = styled.header`
+  background-color: black;
+  color: white;
+`;
+
+function Cabecalho() {
+  return (
+    <StyledHeader>
+      <h1>Olá React!</h1>
+      <hr />
+      <Menu />
+    </StyledHeader>
+  );
 }
 
 export default Cabecalho;
