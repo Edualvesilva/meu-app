fd03466fade682ac37c172606a4be4dd66553d50
Eduardo Alves da Silva <eduardoalvniver@gmail.com>
2023-12-01 11:34:16 -0300

    export na StyleFooter

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,5 @@
-import styled from "styled-components";
-const StyledFooter = styled.footer`
-  background: linear-gradient(to right, #ff7e5f, #feb47b);
-  height: 30vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
+import { StyledFooter } from "../styles/styles.js";
+
 function Footer() {
   return (
     <StyledFooter>
---
+++ src/styles/styles.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+export const StyledFooter = styled.footer`
+  background: linear-gradient(to right, #ff7e5f, #feb47b);
+  height: 30vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
