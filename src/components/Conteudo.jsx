cf735933ed26bff774f43440d56e5614c3fd24b8
Eduardo Alves da Silva <eduardoalvniver@gmail.com>
2023-12-06 09:01:22 -0300

    Exemplo 2 com chamada de função

--- src/components/Conteudo.jsx
+++ src/components/Conteudo.jsx
@@ -4,10 +4,14 @@ import Artigo from "./Artigo";
 import cursos from "../api/cursos.js";
 
 function Conteudo() {
+  /* Função que será chamada a partir do evento acionado por um componente/elemento. */
+  const exemplo2 = () => {
+    alert("Exemplo 2");
+  };
   return (
     <StyleMain>
       <h2>Conteúdo da aplicação</h2>
-
+      <button onClick={exemplo2}>Exemplo 2</button>
       <p>
         Lorem ipsum dolor sit amet consectetur, adipisicing elit. Adipisci
         magnam quo ducimus dolores, necessitatibus earum sit at rem, qui,
