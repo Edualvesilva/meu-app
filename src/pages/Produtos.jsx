1eb4a679ad803f28a783c6b0f94d83f78a1df398
Eduardo Alves da Silva <eduardoalvniver@gmail.com>
2024-01-15 11:42:03 -0300

    Usando a API com useState e UseEffect

--- src/pages/Produtos.jsx
+++ src/pages/Produtos.jsx
@@ -1,34 +1,46 @@
 import { useEffect, useState } from "react";
 function Produtos() {
-    /* O State "produtos" é iniciado como um array vazio.
+  /* O State "produtos" é iniciado como um array vazio.
     Posteriormente (após o carregamento dos dados da API),
     ele será preenchida com os objetos/produtos */
-    const [produtos,setProdutos] = useState([])
+  const [produtos, setProdutos] = useState([]);
 
-    /* Gerenciando o efeito colateral do componente para o carregamento dos dados da API. */
+  /* Gerenciando o efeito colateral do componente para o carregamento dos dados da API. */
 
-    /* Fluxo geral do funcionamento do código abaixo
+  /* Fluxo geral do funcionamento do código abaixo
     1) useEffect é carregado UMA VEZ e APÓS a montagem do page (Produtos)
     2) Em seguida, ele executa a função carregarDados
     3) Ao término dela, atualiza o state (produtos)
     */
 
-    useEffect(()=>{
-        const carregarDados = async () => {
-            try {
-                const resposta = await fetch('https://fakestoreapi.com/products');
-            const dados = await resposta.json();
-            console.log(dados);
-            setProdutos(dados);
-            } catch (error) {
-                console.error("erro: "+error);
-            }
-        }
-        carregarDados();
-    },[])
+  useEffect(() => {
+    const carregarDados = async () => {
+      try {
+        const resposta = await fetch("https://fakestoreapi.com/products");
+        const dados = await resposta.json();
+        console.log(dados);
+        setProdutos(dados);
+      } catch (error) {
+        console.error("erro: " + error);
+      }
+    };
+    carregarDados();
+  }, []);
 
-
-  return <h2>Produtos</h2>;
+  return (
+    <article>
+      <h2>Produtos</h2>
+      {produtos.map((produto) => {
+        return (
+          <section key={produto.id}>
+            <h3>{produto.title}</h3>
+            <p>{produto.price}</p>
+            <p>{produto.description}</p>
+          </section>
+        );
+      })}
+    </article>
+  );
 }
 
 export default Produtos;
