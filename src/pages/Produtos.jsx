5d8033f5d9070757e7a23cd74a9768efef4ecb8e
Eduardo Alves da Silva <eduardoalvniver@gmail.com>
2024-01-15 11:28:37 -0300

    Update Produtos.jsx

--- src/pages/Produtos.jsx
+++ src/pages/Produtos.jsx
@@ -1,22 +1,32 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 function Produtos() {
     /* O State "produtos" é iniciado como um array vazio.
     Posteriormente (após o carregamento dos dados da API),
     ele será preenchida com os objetos/produtos */
     const [produtos,setProdutos] = useState([])
-    const carregarDados = async () => {
-        try {
-            const resposta = await fetch('https://fakestoreapi.com/products');
-        const dados = await resposta.json();
-        console.log(dados);
-        setProdutos(dados);
-        } catch (error) {
-            console.error("erro: "+error);
+
+    /* Gerenciando o efeito colateral do componente para o carregamento dos dados da API. */
+
+    /* Fluxo geral do funcionamento do código abaixo
+    1) useEffect é carregado UMA VEZ e APÓS a montagem do page (Produtos)
+    2) Em seguida, ele executa a função carregarDados
+    3) Ao término dela, atualiza o state (produtos)
+    */
+
+    useEffect(()=>{
+        const carregarDados = async () => {
+            try {
+                const resposta = await fetch('https://fakestoreapi.com/products');
+            const dados = await resposta.json();
+            console.log(dados);
+            setProdutos(dados);
+            } catch (error) {
+                console.error("erro: "+error);
+            }
         }
-    }
+        carregarDados();
+    },[])
 
-
-    carregarDados();
 
   return <h2>Produtos</h2>;
 }
