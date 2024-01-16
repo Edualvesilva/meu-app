4d9fd575fabb9601a4a1fb7f4cf0cd2daedb5835
Eduardo Alves da Silva <eduardoalvniver@gmail.com>
2024-01-16 09:33:45 -0300

    Update DetalhesProduto.jsx

--- src/pages/DetalhesProduto.jsx
+++ src/pages/DetalhesProduto.jsx
@@ -1,9 +1,21 @@
 import { useParams } from "react-router-dom";
-
+import { useEffect } from "react";
 function DetalhesProduto() {
   /* Usamos o Hook useParams do React Router DOM para ter acesso aos parâmetros da rota dinâmica neste caso,o parâmetro chamado "id". */
   const { id } = useParams();
-  console.log(id);
+
+  useEffect(() => {
+    const carregarDados = async () => {
+      try {
+        const resposta = await fetch(
+          `'https://fakestoreapi.com/products/${id}`
+        );
+        const dados = await resposta.json();
+      } catch (error) {
+        console.error("Erro ao carregar produto: " + error);
+      }
+    };
+  }, [id]);
   return (
     <article>
       <h2>Titulo</h2>
